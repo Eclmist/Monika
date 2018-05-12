db66e03261feca2cfd03aa8d86bbc71c551994b7
Samuel Ng <eclmist@yahoo.com>
2018-05-12 14:43:46 +0800

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=monika-ad06-Web-Deploy.pubxml -p:Password=K2vfTHRDA2CvEQYmQrpgTGo2aBllJLqlm7feqdkC5sLty2mWqeC7EdAi0lfL
+
